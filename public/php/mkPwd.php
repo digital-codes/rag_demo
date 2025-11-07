222682b5e8a4e0306c5748bab7ee1da8a7378988
Andreas Kugel <andreas.kugel@web.de>
2025-11-07 22:27:28 +0100

    update

--- public/php/mkPwd.php
+++ public/php/mkPwd.php
@@ -1,4 +1,22 @@
 <?php
-echo (password_hash("12341234", PASSWORD_DEFAULT));
+if (php_sapi_name() !== 'cli') {
+    fwrite(STDERR, "This script must be run from the command line.\n");
+    exit(1);
+}
+
+$pwd = null;
+if (isset($argv[1]) && $argv[1] !== '') {
+    $pwd = $argv[1];
+} else {
+    // prompt for password if not provided as argument
+    $pwd = trim(readline("Password: "));
+}
+
+if ($pwd === '') {
+    fwrite(STDERR, "No password provided.\n");
+    exit(1);
+}
+
+echo password_hash($pwd, PASSWORD_DEFAULT) . PHP_EOL;
 ?>
 
