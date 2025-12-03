66b178482ab84791c681a0410a5b799b986714ea
Andreas Kugel <andreas.kugel@web.de>
2025-12-03 19:47:15 +0100

    specific code for mistral.ai and ollama cloud

--- public/php/remoteLlm.php
+++ public/php/remoteLlm.php
@@ -14,14 +14,22 @@ function remoteQuery($key, $model, $url, $prompt, $query, $temperature = 0.5, $s
     // Prepare request payload. mistral.ai doesn't support 'seed'
     $body = [
         "model" => $model,
-        "messages" => $messages,
-        "temperature" => $temperature,
+        "messages" => $messages
     ];
-    if (stripos($url, 'mistral.ai') === false) {
+    // with temperature for mistral.ai
+    if (stripos($url, 'mistral') !== false) {
+        $body['temperature'] = $temperature;
+    }
+    // with temperature and seed for deepinfra
+    if (stripos($url, 'deepinfra') !== false) {
+        $body['temperature'] = $temperature;
         $body['seed'] = $seed;
     }
+    # no streaming fo rollama 
+    if (stripos($url, 'ollama') !== false) {
+        $body['stream'] = false;
+    }
     $payload = json_encode($body);
-
     // Set up cURL
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -41,7 +49,12 @@ function remoteQuery($key, $model, $url, $prompt, $query, $temperature = 0.5, $s
     // Handle the response
     if ($httpCode === 200) {
         $result = json_decode($response, true);
-        $reply = $result['choices'][0]['message']['content'] ?? "No reply.";
+        // ollama response is different
+        if (stripos($url, 'ollama') !== false) {
+            $reply = $result['message']['content'] ?? "No reply.";
+        } else {
+            $reply = $result['choices'][0]['message']['content'] ?? "No reply.";
+        }
         $result = [
             'status' => 'ok',
             'reply' => $reply,
